ecb4b814c860e474a92d3d0bb39dab8637a39598
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-29 07:44:01 +0900

    add: layer

--- titanic/model.py
+++ titanic/model.py
@@ -4,8 +4,15 @@ import torch.nn as nn
 class Logistic(nn.Module):
     def __init__(self, n_in, n_out):
         super().__init__()
-        self.fc = nn.Linear(n_in, n_out)
+        self.net = nn.Sequential(
+            nn.Linear(n_in, 32),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(16, n_out)
+        )
 
     def forward(self, x):
-        y = self.fc(x)
-        return y
+        return self.net(x)
