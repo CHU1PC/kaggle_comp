73b481dba8adab87fb36b32b0f535e78b03e97c7
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-30 12:44:13 +0900

    add: Embarked to FEATURES

--- titanic/config.py
+++ titanic/config.py
@@ -2,4 +2,4 @@ import os
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
-FEATURES = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]
+FEATURES = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
