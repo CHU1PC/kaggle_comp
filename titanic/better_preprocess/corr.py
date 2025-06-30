59f0dfc79790c2b41b145520e73129bd7e126d7a
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-30 23:21:53 +0900

    change: path

--- titanic/better_preprocess/corr.py
+++ titanic/better_preprocess/corr.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 from config import DATA_DIR
-from kaggle_comp.titanic.better_preprocess.preprocess.main_preprocess import preprocess
+from all_preprocess import preprocess
 
 train_data = pd.read_csv(os.path.join(DATA_DIR, "train.csv"))
 # print(train_data.info())
