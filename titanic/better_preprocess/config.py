0b9d1ccdac68ac66493351a3e934f6bcfa0123cf
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-30 20:52:03 +0900

    delete: name

--- titanic/better_preprocess/config.py
+++ titanic/better_preprocess/config.py
@@ -2,5 +2,5 @@ import os
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
-FEATURES = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked",
-            "Cabin", "Name"]
+FEATURES = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare",
+            "Embarked", "Cabin"]
