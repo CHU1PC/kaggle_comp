265832cfea1d9e7fe3b72ba5b0ce739aa486f237
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-30 12:44:36 +0900

    add: Embarked process

--- titanic/preprocess.py
+++ titanic/preprocess.py
@@ -19,12 +19,24 @@ def preprocess():
     for col in ["Age", "Fare"]:
         train_data[col] = train_data[col].fillna(train_data[col].mean())
         test_data[col] = test_data[col].fillna(test_data[col].mean())
+    train_data["Embarked"] = train_data["Embarked"].fillna("S")
+    test_data["Embarked"] = test_data["Embarked"].fillna("S")
 
     # Sexの数値化
-    train_data["Sex"] = train_data["Sex"].replace({"male": 0, "female": 1})
-    test_data["Sex"] = test_data["Sex"].replace({"male": 0, "female": 1})
+    train_data["Sex"] = train_data["Sex"].\
+        replace({"male": 0, "female": 1}).astype(int)
+    test_data["Sex"] = test_data["Sex"].\
+        replace({"male": 0, "female": 1}).astype(int)
 
-    train_data = train_data.astype(float)
-    test_data = test_data.astype(float)
+    # Embarkedの数値化
+    train_data["Embarked"] = \
+        train_data["Embarked"].replace({"S": 0, "C": 1, "Q": 2}).astype(int)
+    test_data["Embarked"] = \
+        test_data["Embarked"].replace({"S": 0, "C": 1, "Q": 2}).astype(int)
 
     return train_data, test_data
+
+
+# train, test = preprocess()
+
+# print(train.dtypes)
