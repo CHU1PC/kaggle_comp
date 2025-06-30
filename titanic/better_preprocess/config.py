2454b356bb293ccb861f715feeb36d19b893e0e5
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-30 23:13:21 +0900

    add: Family_size

--- titanic/better_preprocess/config.py
+++ titanic/better_preprocess/config.py
@@ -2,5 +2,5 @@ import os
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
-FEATURES = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare",
+FEATURES = ["Pclass", "Sex", "Age", "Family_size", "Fare",
             "Embarked", "Cabin"]
